6bbadf8c825c522712d97b581ea35a792bd77ef1
SafaElmali <tsafaelmali@gmail.com>
2025-01-08 03:04:52 +0300

    feat: Add horizontal rule

--- src/components/features/tiptap-editor/plugins/toolbar/toolbar.tsx
+++ src/components/features/tiptap-editor/plugins/toolbar/toolbar.tsx
@@ -1,5 +1,5 @@
 import { type Editor } from "@tiptap/react";
-import { Bold, Italic, Quote, Redo, Underline, Undo } from "lucide-react";
+import { Bold, Italic, Quote, Redo, Underline, Undo, Minus } from "lucide-react";
 import { FC } from "react";
 import { ToolbarButton } from "./_components/toolbar-button";
 import { HeadingDropdown } from "./_components/heading-dropdown";
@@ -54,6 +54,12 @@ const Toolbar: FC<ToolbarProps> = ({ editor }) => {
           command={() => editor.chain().focus().redo().run()}
           isActive={false}
           disabled={!editor.can().redo()}
+        />
+        <ToolbarButton
+          icon={<Minus className="h-4 w-4" />}
+          label="Horizontal Rule"
+          command={() => editor.chain().focus().setHorizontalRule().run()}
+          isActive={false}
         />
         <ListDropdown editor={editor} />
         <FormattingDropdown editor={editor} />
